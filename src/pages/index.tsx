a4fd6076f48b65f4529e43a5168d8d79fb428db6
ecpieritz <ecpieritz@gmail.com>
2024-06-25 14:34:26 -0300

    add base pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import Dashboard from "./dashboard";
+import Dashboard from "./dashboard/index";
 
 export default function Home() {
   return (
